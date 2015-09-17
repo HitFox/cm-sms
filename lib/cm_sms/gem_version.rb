54b93809d1967cf3370872a67da8b1ec87c78450
itschn <lofd23@yahoo.de>
2015-09-17 16:15:17 +0200

    Increasing minor version

--- lib/cm_sms/gem_version.rb
+++ lib/cm_sms/gem_version.rb
@@ -6,8 +6,8 @@ module CmSms
 
   module VERSION
     MAJOR = 0
-    MINOR = 1
-    TINY  = 4
+    MINOR = 2
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".")
